69a0e5fd6a9ac02b3d8c1fdeb403b8054bd7f195
777stha.satish <777stha.satish@gmail.com>
2020-11-25 09:02:43 +0545

    Initial project creation

---
+++ src/main/java/com/cotiviti/erestaurantbackend/ERestaurantBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cotiviti.erestaurantbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ERestaurantBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ERestaurantBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cotiviti/erestaurantbackend/ERestaurantBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cotiviti.erestaurantbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ERestaurantBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
